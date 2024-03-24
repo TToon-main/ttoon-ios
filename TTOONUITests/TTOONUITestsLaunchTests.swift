3d1fa541613349e454a947e9c81aafb9aed3ac66
lyoodong <dwryu3079@naver.com>
2024-03-24 14:45:47 +0900

    Initial Commit🎉

---
+++ TTOONUITests/TTOONUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TTOONUITestsLaunchTests.swift
+//  TTOONUITests
+//
+//  Created by Dongwan Ryoo on 3/24/24.
+//
+
+import XCTest
+
+final class TTOONUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
