1ab004525265f1a94e2b124b2b1f1a18be8aaff1
류동완입니다 <115209527+lyoodong@users.noreply.github.com>
2024-04-13 16:52:36 +0900

    [TN-96] 디자인 시스템 정의(칼라, 폰트) (#19)

---
+++ TTOON/Util/Extension/UIFont+.swift
@@ -0,0 +1,20 @@
+//
+//  UIFont+.swift
+//  TTOON
+//
+//  Created by Dongwan Ryoo on 4/13/24.
+//
+
+import UIKit
+
+extension UIFont {
+    static let title24b = UIFont(name: TNFont.bold.rawValue, size: 24)!
+    static let title20b = UIFont(name: TNFont.bold.rawValue, size: 20)!
+    static let body16b = UIFont(name: TNFont.bold.rawValue, size: 16)!
+    static let body16m = UIFont(name: TNFont.medium.rawValue, size: 16)!
+    static let body14b = UIFont(name: TNFont.bold.rawValue, size: 14)!
+    static let body14m = UIFont(name: TNFont.medium.rawValue, size: 14)!
+    static let body14r = UIFont(name: TNFont.regular.rawValue, size: 14)!
+    static let body12sb = UIFont(name: TNFont.semiBold.rawValue, size: 12)!
+    static let body12r = UIFont(name: TNFont.regular.rawValue, size: 12)!
+}
