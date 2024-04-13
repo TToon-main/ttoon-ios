0109721c7334d43b3a765d705983e02601a23e9d
lyoodong <dwryu3079@naver.com>
2024-04-13 14:58:16 +0900

    📱폰트 추가

---
+++ TTOON/Util/Constant/TNFont.swift
@@ -0,0 +1,20 @@
+//
+//  Font.swift
+//  TTOON
+//
+//  Created by Dongwan Ryoo on 4/13/24.
+//
+
+import Foundation
+
+enum TNFont: String {
+    case black = "Pretendard-Black"
+    case bold = "Pretendard-Bold"
+    case extraBold = "Pretendard-ExtraBold"
+    case extraLight = "Pretendard-ExtraLight"
+    case light = "Pretendard-Light"
+    case medium = "Pretendard-Medium"
+    case regular = "Pretendard-Regular"
+    case semiBold = "Pretendard-SemiBold"
+    case thin = "Pretendard-Thin"
+}
