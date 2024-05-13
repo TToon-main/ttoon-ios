ba1c5414509dd2c47681bdbc1987b987a6f43921
limsss9076@naver.com <limsss9076@naver.com>
2024-05-13 16:05:16 +0900

    ✨서버 통신 결과 VM으로 전달 완료

--- TTOON/Data/Repository/Auth/LoginRepository.swift
+++ TTOON/Data/Repository/Auth/LoginRepository.swift
@@ -151,6 +151,50 @@ extension LoginRepository: ASAuthorizationControllerDelegate {
     // apple login failure
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
         print("login failure")
+    }
+}
+
+
+// MARK: - Login API Request
+extension LoginRepository {
+    func loginRequest(_ requestDTO: LoginRequestDTO) {
+        self.provider.unAuthProvider.request(
+            .socialLogin(dto: requestDTO)) { result in
+                switch result {
+                case .success(let response):
+                    // 통신을 해보면, 성공 실패 모두 여기로 응답이 온다. (200, 401)
+                    
+                    print("statusCode : ", response.statusCode)
+                    
+                    // 성공
+                    if let data = try? response.map(ResponseSuccessDTO<LoginResponseDTO>.self) {
+                        print("성공 : ", data)
+                        
+                        // 결과 VM으로 전달
+                        self.loginResult.onNext(.success(data.data.toDomain()))
+                    }
+                    
+                    // 실패
+                    else {
+                        // 결과 VM으로 전달
+                        self.loginResult.onNext(.failure(SampleError.a))
+                    }
+                        
+                case .failure(let error):
+                
+                    print(error.localizedDescription)
+                    
+                    // 결과 VM으로 전달
+                    self.loginResult.onNext(.failure(error))
+                }
+        }
+    }
+}
+
+
+// MARK: - private func
+extension LoginRepository {
+    // jwt token decoding
         
         self.loginResult.onNext(.failure(LoginError.appleError))
     }
@@ -192,6 +236,34 @@ extension LoginRepository {
 // MARK: - private func
 extension LoginRepository {
     // decode email from JWT
+    private func decode(jwtToken jwt: String) -> [String: Any] {
+        func base64UrlDecode(_ value: String) -> Data? {
+            var base64 = value
+                .replacingOccurrences(of: "-", with: "+")
+                .replacingOccurrences(of: "_", with: "/")
+
+            let length = Double(base64.lengthOfBytes(using: String.Encoding.utf8))
+            let requiredLength = 4 * ceil(length / 4.0)
+            let paddingLength = requiredLength - length
+            if paddingLength > 0 {
+                let padding = "".padding(toLength: Int(paddingLength), withPad: "=", startingAt: 0)
+//                base64 = base64 + padding
+                base64 += padding
+            }
+            return Data(base64Encoded: base64, options: .ignoreUnknownCharacters)
+        }
+
+        func decodeJWTPart(_ value: String) -> [String: Any]? {
+            guard let bodyData = base64UrlDecode(value),
+                  let json = try? JSONSerialization.jsonObject(with: bodyData, options: []), let payload = json as? [String: Any] else {
+                return nil
+            }
+
+            return payload
+        }
+        
+        let segments = jwt.components(separatedBy: ".")
+        return decodeJWTPart(segments[1]) ?? [:]
     private func decode(jwtToken jwt: String) -> [String: Any] {
         func base64UrlDecode(_ value: String) -> Data? {
             var base64 = value
