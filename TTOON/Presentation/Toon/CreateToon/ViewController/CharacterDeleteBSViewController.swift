8ef7530a4b37a3e23a15c846e4ec36aaecd5560b
lyoodong <dwryu3079@naver.com>
2024-07-14 00:43:12 +0900

    💄파일 생성

---
+++ TTOON/Presentation/Toon/CreateToon/ViewController/CharacterDeleteBSViewController.swift
@@ -0,0 +1,57 @@
+//
+//  CharacterDeleteBSViewController.swift
+//  TTOON
+//
+//  Created by Dongwan Ryoo on 7/14/24.
+//
+
+import UIKit
+
+import ReactorKit
+import RxCocoa
+import RxSwift
+
+// protocol PresentModifyCharacterVCDelegate: AnyObject {
+//    func presentModifyCharacterViewController()
+// }
+
+class CharacterDeleteBSViewController: BaseViewController {
+    var disposeBag = DisposeBag()
+    private let characterDeleteBSView = CharacterDeleteBSView()
+    weak var delegate: PresentModifyCharacterVCDelegate?
+    
+    init(reactor: CharacterDeleteBSReactor) {
+        super.init(nibName: nil, bundle: nil)
+        self.reactor = reactor
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        view = characterDeleteBSView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindMockUp()
+    }
+    
+    func bindMockUp() {
+    }
+}
+
+extension CharacterDeleteBSViewController: View {
+    func bind(reactor: CharacterDeleteBSReactor) {
+        bindAction(reactor: reactor)
+        bindState(reactor: reactor)
+    }
+    
+    func bindAction(reactor: CharacterDeleteBSReactor) {
+    }
+    
+    func bindState(reactor: CharacterDeleteBSReactor) {
+    }
+}
