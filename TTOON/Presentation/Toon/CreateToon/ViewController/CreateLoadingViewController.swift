0476753960ea5fa233a83cad29b05940a06dc4af
lyoodong <dwryu3079@naver.com>
2024-07-27 20:48:35 +0900

    👔 비지니스 로직 추가

--- TTOON/Presentation/Toon/CreateToon/ViewController/CreateLoadingViewController.swift
+++ TTOON/Presentation/Toon/CreateToon/ViewController/CreateLoadingViewController.swift
@@ -7,14 +7,63 @@
 
 import UIKit
 
+import ReactorKit
+import RxCocoa
 import RxSwift
 
 class CreateLoadingViewController: BaseViewController {
-    private let disposeBag = DisposeBag()
+    var disposeBag = DisposeBag()
     
+    private let viewLifeCycle = PublishSubject<CreateLoadingReactor.Action>()
     private let createLoadingView = CreateLoadingView()
+    
+    init(reactor: CreateLoadingReactor) {
+        super.init(nibName: nil, bundle: nil)
+        self.reactor = reactor
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func loadView() {
         view = createLoadingView
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewLifeCycle.onNext(.viewLifeCycle(cycle: .viewWillAppear))
+    }
+}
+
+extension CreateLoadingViewController: View {
+    func bind(reactor: CreateLoadingReactor) {
+        bindAction(reactor: reactor)
+        bindState(reactor: reactor)
+    }
+    
+    func bindAction(reactor: CreateLoadingReactor) {
+        viewLifeCycle
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+    }
+    
+    func bindState(reactor: CreateLoadingReactor) {
+        reactor.state
+            .map { $0.currentProgress }
+            .bind(to: createLoadingView.rx.currentProgress)
+            .disposed(by: disposeBag)
+        
+        reactor.state
+            .map { $0.completeProgress }
+            .compactMap { $0 }
+            .bind(onNext: presentCreateResultVC)
+            .disposed(by: disposeBag)
+    }
+    
+    func presentCreateResultVC() {
+        let vc = CreateResultViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
     }
 }
---
+++ TTOON/Util/Enums/Gloabal/ViewLifeCycle.swift
@@ -0,0 +1,19 @@
+//
+//  ViewLifeCycle.swift
+//  TTOON
+//
+//  Created by Dongwan Ryoo on 7/27/24.
+//
+
+import Foundation
+
+enum ViewLifeCycle {
+    case loadView
+    case viewDidLoad
+    case viewWillAppear
+    case viewDidAppear
+    case viewWillDisappear
+    case viewDidDisappear
+    case viewWillLayoutSubviews
+    case viewDidLayoutSubviews
+}
