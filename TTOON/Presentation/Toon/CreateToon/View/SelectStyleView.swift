461ff637b500523841d056f89ec3eefae65790f9
류동완입니다 <115209527+lyoodong@users.noreply.github.com>
2024-07-27 19:23:25 +0900

    [SUB] 그림체 스타일 선택 UI 작업 (#33)

---
+++ TTOON/Presentation/Toon/CreateToon/View/SelectStyleView.swift
@@ -0,0 +1,80 @@
+//
+//  SelectStyleView.swift
+//  TTOON
+//
+//  Created by Dongwan Ryoo on 6/21/24.
+//
+
+import UIKit
+
+import FlexLayout
+import PinLayout
+import RxCocoa
+import RxSwift
+
+class SelectStyleView: BaseView {
+    let titleLabel = {
+        let view = CreateToonTitleLabel()
+        view.text = "가장 마음에 드는 그림체를\n선택해주세요"
+        
+        return view
+    }()
+    
+    let subTitleLabel = {
+        let view = CreateToonSubTitleLabel()
+        view.text = "골라주신 그림체로 네컷만화를\n생성해드릴게요"
+        
+        return view
+    }()
+    
+    
+    let createToonCollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 106, height: 132)
+        layout.minimumLineSpacing = 12
+        layout.minimumInteritemSpacing = 12
+        
+        let view = CreateToonCollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        return view
+    }()
+    
+    let createToonConfirmButton = {
+        let view = TNButton()
+        view.isEnabled = false
+        view.setTitle("완료", for: .normal)
+        
+        return view
+    }()
+    
+    override func layouts() {
+        flex.direction(.column).padding(16).define { (flex) in
+            flex.addItem(titleLabel)
+            flex.addItem(subTitleLabel)
+            flex.addItem(createToonCollectionView)
+            flex.addItem(createToonConfirmButton)
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        titleLabel.pin.top(safeAreaInsets.top + 36).left(16).sizeToFit()
+        subTitleLabel.pin.below(of: titleLabel).marginTop(16).left(16).sizeToFit()
+        createToonCollectionView.pin.below(of: subTitleLabel).marginTop(84).horizontally(16).height(132)
+        createToonConfirmButton.pin.bottom(safeAreaInsets.bottom + 36).horizontally(16).height(56)
+    }
+}
+
+extension Reactive where Base: SelectStyleView {
+    var modelSelected: Observable<SelectStyleReactor.Action> {
+        return base.createToonCollectionView.rx.modelSelected(String.self)
+            .map { _ in SelectStyleReactor.Action.modelSelected }
+    }
+}
+
+extension Reactive where Base: SelectStyleView {
+    var isEnabledConfirmButton: Binder<Bool> {
+        return base.createToonConfirmButton.rx.isEnabled
+    }
+}
---
+++ TTOON/Presentation/Toon/CreateToon/ViewController/SelectStyleViewController.swift
@@ -0,0 +1,70 @@
+//
+//  SelectStyleViewController.swift
+//  TTOON
+//
+//  Created by Dongwan Ryoo on 6/21/24.
+//
+
+import UIKit
+
+import ReactorKit
+import RxCocoa
+import RxSwift
+
+class SelectStyleViewController: CreateToonBaseViewController {
+    private let selectStyleView = SelectStyleView()
+    var disposeBag = DisposeBag()
+    
+    init(reactor: SelectStyleReactor) {
+        super.init(nibName: nil, bundle: nil)
+        self.reactor = reactor
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        view = selectStyleView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindMockData()
+    }
+}
+extension SelectStyleViewController: View {
+    func bindMockData() {
+        let cells = ["그림체 1", "그림체 2", "그림체 3"]
+        let items = BehaviorSubject(value: cells)
+        
+        // CollectionView 바인딩
+        items.bind(to: selectStyleView.createToonCollectionView.rx
+            .items(cellIdentifier: CreateToonCollectionViewCell.IDF, 
+                   cellType: CreateToonCollectionViewCell.self)) 
+        { index, item, cell in 
+            cell.titleLabel.text = item
+        }
+        .disposed(by: disposeBag)
+    }
+    
+    func bind(reactor: SelectStyleReactor) {
+        bindAction(reactor: reactor)
+        bindState(reactor: reactor)
+    }
+    
+    func bindAction(reactor: SelectStyleReactor) {
+        selectStyleView.rx
+            .modelSelected
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+    }
+    
+    func bindState(reactor: SelectStyleReactor) {
+        reactor.state
+            .map { $0.isEnabledConfirmButton }
+            .bind(to: selectStyleView.rx.isEnabledConfirmButton)
+            .disposed(by: disposeBag)
+    }
+}
