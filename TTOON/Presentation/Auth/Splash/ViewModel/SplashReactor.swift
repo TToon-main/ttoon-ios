965c50c8650812b5b74bf537f6e05e585f4b56fc
lyoodong <dwryu3079@naver.com>
2024-08-18 22:58:09 +0900

    💄스플래시 로고 변경, 서버 점검 여부 로직 제거

--- TTOON/Presentation/Auth/Splash/ViewModel/SplashReactor.swift
+++ TTOON/Presentation/Auth/Splash/ViewModel/SplashReactor.swift
@@ -44,11 +44,44 @@ final class SplashReactor: Reactor {
     let initialState: State = State()
     
     func mutate(action: Action) -> Observable<Mutation> {
-        switch action {
-        case .viewDidLoad:
-            return checkSplashStatus().map { status in
-                return Mutation.setSplashStatus(status)
-            }
+          switch action {
+          case .viewDidLoad:
+              return .just(.setSplashStatus(checkSplashStatus()))
+          }
+      }
+        
+      // 사실상 ViewController에 Output 전달이 필요하지 않아 보인다.
+      func reduce(state: State, mutation: Mutation) -> State {
+          var newState = state
+          switch mutation {
+          case .setSplashStatus(let status):
+              newState.splashStatus = status
+          }
+          return newState
+      }
+    
+    
+    // 위치가 적절한 지는 의문이지만, 여기서 코디로 이벤트 전달!
+    private func checkSplashStatus() -> SplashStatus {
+        let isConnect = splashUseCase.isNetworkConnected()
+        let isNeedUpdate = splashUseCase.isMinVersionReached()
+
+        
+        if isConnect {
+            sendTransitionEvent?(.goSplashErrorView(.disConnected))
+            return .disConnected 
+        } else if isNeedUpdate {
+            sendTransitionEvent?(.goSplashErrorView(.needUpdate))
+            return .needUpdate
+        } else {
+            // 여기서 토큰 여부 확인! (기존 VC에 있던 코드)
+            let isTokenValid = false
+            
+            sendTransitionEvent?( 
+                (isTokenValid) ? .goHomeView : .goLoginView
+            )
+            
+            return .valid
         }
     }
     
