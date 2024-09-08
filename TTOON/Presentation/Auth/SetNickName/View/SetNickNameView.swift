0d9badbe6ba35177f600887154846f645f6a8e97
lyoodong <dwryu3079@naver.com>
2024-09-08 23:06:19 +0900

    💄닉네임 설정 UI 작업

--- TTOON/Presentation/Auth/SetNickName/View/SetNickNameView.swift
+++ TTOON/Presentation/Auth/SetNickName/View/SetNickNameView.swift
@@ -5,4 +5,73 @@
 //  Created by Dongwan Ryoo on 9/8/24.
 //
 
-import Foundation
+import UIKit
+
+class SetNickNameView: BaseView {
+    let titleLabel = {
+        let view = CreateToonTitleLabel()
+        view.text = "닉네임을 입력해주세요"
+        
+        return view
+    }()
+    
+    let textFiledTitleLabel = {
+        let view = UILabel()
+        view.text = "닉네임"
+        view.textColor = .grey06
+        view.font = .body14m
+        
+        return view
+    }()
+    
+    let textField = {
+        let view = TNTextFiled()
+        view.textFiled.placeholder = "닉네임 (최대 10자)"
+        view.textFiled.clearButtonMode = .always
+        
+        return view
+    }()
+    
+    let confirmButton = {
+        let view = TNButton()
+        view.setTitle("저장", for: .normal)
+        view.isEnabled = false
+        
+        return view
+    }()
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        endEditing(true)
+    }
+    
+    override func addSubViews() {
+        addSubview(titleLabel)
+        addSubview(textFiledTitleLabel)
+        addSubview(textField)
+        addSubview(confirmButton)
+    }
+    
+    override func layouts() {
+        titleLabel.snp.makeConstraints { 
+            $0.top.equalToSuperview().offset(39)
+            $0.leading.equalToSuperview().offset(16)
+        }
+        
+        textFiledTitleLabel.snp.makeConstraints { 
+            $0.top.equalTo(titleLabel.snp.bottom).offset(40)
+            $0.leading.equalToSuperview().offset(16)
+        }
+        
+        textField.snp.makeConstraints { 
+            $0.top.equalTo(textFiledTitleLabel.snp.bottom).offset(8)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        confirmButton.snp.makeConstraints { 
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            $0.height.equalTo(56)
+            $0.bottom.equalTo(safeGuide).offset(-36)
+        }
+    }
+}
--- TTOON/Presentation/Auth/SetNickName/ViewController/SetNickNameViewController.swift
+++ TTOON/Presentation/Auth/SetNickName/ViewController/SetNickNameViewController.swift
@@ -5,4 +5,14 @@
 //  Created by Dongwan Ryoo on 9/8/24.
 //
 
-import Foundation
+import UIKit
+
+import RxSwift
+
+class SetNickNameViewController: BaseViewController {
+    private let setNickNameView = SetNickNameView()
+    
+    override func loadView() {
+        view = setNickNameView
+    }
+}
