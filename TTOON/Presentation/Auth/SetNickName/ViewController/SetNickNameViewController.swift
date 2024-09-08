e385b8ace87466f1c2eb53e61a44d1d5f3b6d5b0
lyoodong <dwryu3079@naver.com>
2024-09-08 22:52:34 +0900

    🌱닉네임 feature 파일 생성

---
+++ TTOON/Presentation/Auth/SetNickName/ViewController/SetNickNameViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SetNickNameViewController.swift
+//  TTOON
+//
+//  Created by Dongwan Ryoo on 9/8/24.
+//
+
+import Foundation
---
+++ TTOON/Presentation/Auth/SetNickName/ViewModel/SetNickNameReactor.swift
@@ -0,0 +1,8 @@
+//
+//  SetNickNameReactor.swift
+//  TTOON
+//
+//  Created by Dongwan Ryoo on 9/8/24.
+//
+
+import Foundation
