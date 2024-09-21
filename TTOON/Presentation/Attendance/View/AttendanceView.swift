33ab7ba5157657ca343834c2ed2f0b3c74fab419
lyoodong <dwryu3079@naver.com>
2024-09-21 22:20:19 +0900

    💄안내 텍스트 추가

--- TTOON/Presentation/Attendance/View/AttendanceView.swift
+++ TTOON/Presentation/Attendance/View/AttendanceView.swift
@@ -14,8 +14,28 @@ class AttendanceView: BaseView {
         return view
     }()
     
+    let titleLabel = {
+        let view = UILabel()
+        view.textColor = .black
+        view.font = .title24b
+        view.numberOfLines = 0
+        view.text = "매일 출석 체크하고\n포인트를 모아보아요"
+        
+        return view
+    }()
+    
+    let subTitleLabel = {
+        let view = UILabel()
+        view.textColor = .grey06
+        view.font = .body16m
+        view.numberOfLines = 0
+        view.text = "모은 포인트로 만화를 다시\n생성할 수 있어요"
+        
+        return view
+    }()
+    
     override func addSubViews() {
-        [navigationBar].forEach { view in
+        [navigationBar, titleLabel, subTitleLabel].forEach { view in
             self.addSubview(view)
         }
     }
@@ -26,6 +46,16 @@ class AttendanceView: BaseView {
         navigationBar.snp.makeConstraints { 
             $0.top.equalTo(safeGuide)
             $0.centerX.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints { 
+            $0.top.equalTo(navigationBar.snp.bottom).offset(8)
+            $0.leading.equalToSuperview().offset(16)
+        }
+        
+        subTitleLabel.snp.makeConstraints { 
+            $0.top.equalTo(titleLabel.snp.bottom).offset(12)
+            $0.leading.equalToSuperview().offset(16)
         }
     }
 }
