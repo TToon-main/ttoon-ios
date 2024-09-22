86b8434237f890f369a505a96b6035f1297cbb9e
lyoodong <dwryu3079@naver.com>
2024-09-22 22:40:33 +0900

    🌱출석 체크 network 세팅 및 출석 체크 조회 기능 추가

---
+++ TTOON/Data/DTO/Attendance/AttendanceDTO.swift
@@ -0,0 +1,30 @@
+//
+//  AttendanceDTO.swift
+//  TTOON
+//
+//  Created by Dongwan Ryoo on 9/22/24.
+//
+
+import Foundation
+
+// 출석 체크 조회 API
+
+struct GetAttendanceResponseDTO: Codable {
+    let point: Int
+    let dayStatuses: [DayStatus]
+    
+    struct DayStatus: Codable {
+        let day: String
+        let isAttended: Bool
+        
+        enum CodingKeys: String, CodingKey {
+            case day = "dayOfWeek"
+            case isAttended = "status"
+        }
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case point
+        case dayStatuses = "AttendDtoList"
+    }
+}
