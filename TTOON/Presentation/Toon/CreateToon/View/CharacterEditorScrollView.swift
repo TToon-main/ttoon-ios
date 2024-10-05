95e277ed66b30fb0324f1ddde71792364346d6b8
lyoodong <dwryu3079@naver.com>
2024-10-05 17:06:31 +0900

    💄캐릭터 추가 화면 레이아웃 조정 및 I/O 정의

---
+++ TTOON/Presentation/Toon/CreateToon/View/CharacterEditorScrollView.swift
@@ -0,0 +1,101 @@
+//
+//  CharacterEditorScrollView.swift
+//  TTOON
+//
+//  Created by Dongwan Ryoo on 10/5/24.
+//
+
+import UIKit
+
+import FlexLayout
+import PinLayout
+import RxCocoa
+import RxSwift
+
+class CharacterEditorScrollView: BaseView {
+    // MARK: - UI Properties
+    
+    let characterEditorView = CharacterEditorView()
+    
+    private let scrollView = {
+        let view = UIScrollView()
+        view.showsHorizontalScrollIndicator = false
+        view.backgroundColor = .white
+        
+        return view
+    }()
+    
+    // MARK: - Configurations
+    
+    override func addSubViews() {
+        addSubview(scrollView)
+        scrollView.addSubview(characterEditorView)
+    }
+    
+    override func layouts() {
+        scrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        characterEditorView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalTo(width)
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        scrollView.contentSize = characterEditorView.bounds.size
+    }
+}
+
+// MARK: - Custom Observable
+
+extension Reactive where Base: CharacterEditorScrollView {
+    var nameTextDidChange: Observable<String> {
+        return base.characterEditorView.nameInputView.rx.textDidChanged
+    }
+    
+    var infoTextDidChange: Observable<String> {
+        return base.characterEditorView.diaryInputTextView.rx.text
+            .asObservable()
+            .map({ text in
+                let isPlaceholder = text == base.characterEditorView.placeholderText
+                return isPlaceholder ? "" : text
+            })
+            .compactMap { $0 }
+    }
+    
+    var isSwitchOn: Observable<Bool> {
+        return base.characterEditorView.switchView.switchView.rx.isOn
+            .asObservable()
+    }
+}
+
+// MARK: - Custom Binder
+
+extension Reactive where Base: CharacterEditorScrollView {
+    var nameTextFiledCountLabel: Binder<String?> {
+        return base.characterEditorView.nameInputView.rx.cntText
+    }
+    
+    var infoTextFiledCountLabel: Binder<String?> {
+        return base.characterEditorView.dairyLimitTextLabel.rx.cntText
+    }
+    
+    var errorNameTextFiled: Binder<String?> {
+        return base.characterEditorView.nameInputView.rx.errorMassage
+    }
+    
+    var errorInfoTextFiled: Binder<String?> {
+        return base.characterEditorView.dairyLimitTextLabel.rx.errorMassage
+    }
+    
+    var isOn: Binder<Bool> {
+        return base.characterEditorView.switchView.switchView.rx.isOn
+    }
+    
+    var isEnabledConfirmButton: Binder<Bool> {
+        return base.characterEditorView.confirmButton.rx.isEnabled
+    }
+}
