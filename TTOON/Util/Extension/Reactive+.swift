ac04e625d3a6c17ec2c9d65c87434183d262bbde
limsss9076@naver.com <limsss9076@naver.com>
2024-10-10 21:26:48 +0900

    ♻️ 리뷰 반영 (pagination, 중복 코드 제거)

--- TTOON/Util/Extension/Reactive+.swift
+++ TTOON/Util/Extension/Reactive+.swift
@@ -84,3 +84,19 @@ extension Reactive where Base: UIViewController {
         return ControlEvent(events: source).asObservable()
     }
 }
+
+extension Reactive where Base: UIScrollView {
+    func reachedBottom(offset: CGFloat = 0.0) -> Observable<Void> {
+        let source = contentOffset.map { contentOffset in
+            let visibleHeight = self.base.frame.height - self.base.contentInset.top - self.base.contentInset.bottom
+            let y = contentOffset.y + self.base.contentInset.top
+            let threshold = max(offset, self.base.contentSize.height - visibleHeight - offset)
+            return y >= threshold
+        }
+        .distinctUntilChanged()
+        .filter { $0 }
+        .map { _ in () }
+        
+        return ControlEvent(events: source).asObservable()
+    }
+}
