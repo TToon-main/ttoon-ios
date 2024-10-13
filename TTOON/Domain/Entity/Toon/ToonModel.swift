930b497002549e90589ffd7ae662cc0644c788c7
lyoodong <dwryu3079@naver.com>
2024-10-13 15:01:24 +0900

    👔 view의 데이터 참조해 요청하는 로직 추가

--- TTOON/Domain/Entity/Toon/ToonModel.swift
+++ TTOON/Domain/Entity/Toon/ToonModel.swift
@@ -53,7 +53,7 @@ struct ModifyCharacter {
         return .init(id: id, name: name, info: info)
     }
 }
-
+    
 struct CreateToon {
     let mainCharacterId: Int64
     let others: [Int64]
@@ -67,5 +67,17 @@ struct CreateToon {
                      number: number,
                      title: title,
                      content: content)
+    }
+    
+    func mockUp() -> Self {
+        let model = CreateToon.init(
+            mainCharacterId: 19,
+            others: [20, 21],
+            number: 3,
+            title: "오늘의 일기",
+            content: "오늘 친구들과 강남역에서 만나 커피를 마시고 책을 보며 시간을 보냈다. 다음 여행지로 부산을 정하고, 어디를 갈지 함께 이야기했다. 맛집과 명소들을 알아보며 설레는 마음으로 계획을 세웠고, 이동은 기차를 타기로 결정했다. 부산에서의 새로운 추억이 기대된다."
+        )
+        
+        return model
     }
 }
