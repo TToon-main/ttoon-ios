10c768c1133cd9428545e0aa4a68e61cce1daf90
류동완입니다. <115209527+lyoodong@users.noreply.github.com>
2024-11-03 16:09:08 +0900

    [TN-212] 캐릭터 조회(READ) 비지니스 로직  (#62)

--- TTOON/Data/DTO/Toon/ToonResponseDTO.swift
+++ TTOON/Data/DTO/Toon/ToonResponseDTO.swift
@@ -8,7 +8,16 @@
 import Foundation
 
 struct GetCharacterResponseDTO: Codable {
-    let id: String
+    let id: Int64
     let name: String
     let info: String
+    
+    func toDomain(mainId: String) -> Character {
+        let isMain = "\(id)" == mainId
+        
+        return .init(isMain: isMain,
+                     id: "\(id)",
+                     name: name,
+                     info: info)
+    }
 }
